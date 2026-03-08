bf70e7f0127a0a92a4931894c296cacce8d0e343
OmarAlyEE <1omargaber@gmail.com>
2026-03-08 11:00:37 -0500

    Update test data schema in validation tests

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -1,12 +1,10 @@
-# tests/test_data_validation.py
 import pandas as pd
 from src.data_validation import validate_data
 
 def test_schema():
     df = pd.DataFrame({
-        "timestamp": ["2026-03-01 00:00", "2026-03-01 01:00"],
-        "energy_demand": [100.5, 110.3],
-        "temperature": [22.1, 21.8],
-        "humidity": [45, 50],
+        "Voltage": [234.0, 235.0],
+        "Global_intensity": [18.0, 20.0],
+        "Global_active_power": [4.2, 4.5]
     })
     validate_data(df)
