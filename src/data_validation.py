888abe184b5dbaee29e79893c6ebee4ac486cf2d
OmarAlyEE <1omargaber@gmail.com>
2026-03-08 11:27:08 -0500

    Refactor data validation schema and function

--- src/data_validation.py
+++ src/data_validation.py
@@ -1,17 +1,11 @@
-
-# src/data_validation.py
-import pandas as pd
 import pandera as pa
-from pandera import Column, DataFrameSchema, Check
+from pandera import Column, DataFrameSchema
 
-# Example schema for hourly energy data
-schema = pa.DataFrameSchema({
-    "timestamp": pa.Column(str),
-    "energy_demand": pa.Column(float),
-    "temperature": pa.Column(float),
-    "humidity": pa.Column(int),
+schema = DataFrameSchema({
+    "Voltage": Column(float, nullable=False),
+    "Global_intensity": Column(float, nullable=False),
+    "Global_active_power": Column(float, nullable=False),
 })
 
-def validate_data(df: pd.DataFrame):
-    schema.validate(df, lazy=True)  # lazy=True collects all errors
-    print("✅ Data validation passed!")
+def validate_data(df):
+    schema.validate(df, lazy=True)
